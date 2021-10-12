b39b9d7d66e001065a0bf3e68dfba05a29f19d20
Assad <astb01@gmail.com>
2021-10-12 14:42:45 +0100

    Add form to new travel blog

--- src/pages/NewTravelBlog.js
+++ src/pages/NewTravelBlog.js
@@ -1,9 +1,12 @@
 import React from "react";
+import NewTravelBlogForm from "../components/blogs/NewTravelBlogForm";
 
 const NewTravelBlog = () => {
   return (
     <div>
       <h1>New Travel Blog</h1>
+
+      <NewTravelBlogForm />
     </div>
   );
 };
