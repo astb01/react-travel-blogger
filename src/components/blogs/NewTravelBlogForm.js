c70899a1c5c9be17862648fc135b6856f6413a4f
Assad <astb01@gmail.com>
2021-10-12 15:06:29 +0100

    add useref

--- src/components/blogs/NewTravelBlogForm.js
+++ src/components/blogs/NewTravelBlogForm.js
@@ -1,31 +1,53 @@
-import React from "react";
+import { useRef } from "react";
 import Card from "../ui/Card";
 
 import classes from "./NewTravelBlogForm.module.css";
 
 const NewTravelBlogForm = () => {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const cityInputRef = useRef();
+  const descriptionRef = useRef();
+
+  const submitHandler = (event) => {
+    // prevent refresh on submit:
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImageUrl = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+    const enteredDescription = descriptionRef.current.value;
+  };
+
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" required />
+          <input type="text" id="title" required ref={titleInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Image URL</label>
-          <input type="url" id="image" required />
+          <input type="url" id="image" required ref={imageInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" id="address" required />
+          <input type="text" id="address" required ref={addressInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor="city">Address</label>
-          <input type="text" id="city" required />
+          <label htmlFor="city">City</label>
+          <input type="text" id="city" required ref={cityInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
-          <textarea id="description" required rows="5"></textarea>
+          <textarea
+            id="description"
+            required
+            rows="5"
+            ref={descriptionRef}
+          ></textarea>
         </div>
         <div className={classes.actions}>
           <button>Add Blog</button>
