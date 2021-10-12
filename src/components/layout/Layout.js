8ffe0ba6dff3d70913b160bc420a4c5c5c484652
Assad <astb01@gmail.com>
2021-10-12 13:37:52 +0100

    Add layout style

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,10 +1,14 @@
 import React from "react";
 import MainNavigation from "./MainNavigation";
 
+import classes from "./Layout.module.css";
+
 const Layout = () => {
   return (
     <div>
       <MainNavigation />
+
+      <main className={classes.main}></main>
     </div>
   );
 };
